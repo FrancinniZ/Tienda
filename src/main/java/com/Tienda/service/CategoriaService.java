0ee7c0095097696d50dd730e5865d41a5f18a504
Francinni ZC <113738269+FrancinniZ@users.noreply.github.com>
2023-10-17 21:08:27 -0600

    Semana5

--- src/main/java/com/Tienda/service/CategoriaService.java
+++ src/main/java/com/Tienda/service/CategoriaService.java
@@ -5,8 +5,19 @@ import java.util.List;
 
 public interface CategoriaService {
     
+    
+   // retorna una lista de categorias (activas o todas)
     public List<Categoria> getCategorias(boolean activos);
     
+    // retorna una categoria por id
     public Categoria getCategoria(Categoria categoria);
-
+    
+    //Se inserta un nuevo registro si el id de la categoria está vacío
+    //Se actualiza el registro si el id de la categoria NO está vacío
+    public void save (Categoria categoria);
+    
+    public void delete(Categoria categoria);
+    
+    
+    
 }
--- src/main/java/com/Tienda/service/impl/CategoriaServiceImpl.java
+++ src/main/java/com/Tienda/service/impl/CategoriaServiceImpl.java
@@ -8,19 +8,19 @@ import com.Tienda.service.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
-
 @Service
-public class CategoriaServiceImpl implements CategoriaService { 
-    
+public class CategoriaServiceImpl implements CategoriaService {
+
     @Autowired
     private CategoriaDao categoriaDao;
-    
+
     @Override
     @Transactional(readOnly = true)
     public List<Categoria> getCategorias(boolean activos) {
         List<Categoria> categorias = categoriaDao.findAll();
-        if (activos) 
+        if (activos) {
             categorias.removeIf(c -> !c.isActivo());
+        }
         return categorias;
     }
 
@@ -29,6 +29,16 @@ public class CategoriaServiceImpl implements CategoriaService {
         return categoriaDao.findById(categoria.getIdCategoria()).orElse(null);
     }
 
-   
-    
+    @Override
+    @Transactional
+    public void save(Categoria categoria) {
+        categoriaDao.save(categoria);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Categoria categoria) {
+        categoriaDao.delete(categoria);
+    }
 }
+
